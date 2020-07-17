0fe36e795fcfed0912f08b454f3e3376de57d599
gabriela <gubriana@gmail.com>
2020-07-16 23:49:21 -0400

    Ejercicio contador de clicks en VUE

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png"><br>
+    <br>
+    <ContadorClicks />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ContadorClicks from './components/ContadorClicks.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ContadorClicks
   }
 }
+
 </script>
 
 <style>
---
+++ src/components/ContadorClicks.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h2>Contador de CLICKS</h2>
+   <button @click="onClick()">click!</button><br>
+    <p>Número de CLICKS = {{timesClicked}} </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ContadorClicks',
+  data() {
+    return{
+      timesClicked: 0
+    }
+  },
+  methods: {
+    onClick(){
+      this.timesClicked++;
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
